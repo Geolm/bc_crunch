f818e79837c8a1fd9cb30b19dcde42123879485a
Geolm <moyaguillaume@gmail.com>
2025-11-11 14:44:14 +0100

    block mode try

--- bc_crunch.c
+++ bc_crunch.c
@@ -442,11 +442,10 @@ size_t bc1_crunch(const void* input, uint32_t width, uint32_t height, void* outp
         model_init(&blue[i],  1 << BLUE_NUM_BITS);
     }
 
-    range_model table_index;
+    range_model table_index, table_difference, block_mode;
     model_init(&table_index, top_table_size);
-
-    range_model table_difference;
     model_init(&table_difference, 256);
+    model_init(&block_mode, 4);
 
     bc1_block empty_block = {0};
     bc1_block* previous = &empty_block;
@@ -477,10 +476,35 @@ size_t bc1_crunch(const void* input, uint32_t width, uint32_t height, void* outp
 
             // xor the difference and encode (could be 0 if equal to reference)
             uint32_t difference = current->indices ^ top_table[reference].key;
+            uint32_t hamming = __builtin_popcount(difference);
+            
+            if (hamming == 0)
+            {
+                enc_put(&codec, &block_mode, 0);
+            }
+            else if (hamming == 1)
+            {
+                enc_put(&codec, &block_mode, 1);
+                enc_put_bits(&codec, 31 - __builtin_clz(difference), 5);
+            }
+            else if (hamming == 2)
+            {
+                enc_put(&codec, &block_mode, 2);
 
-            // encode difference
-            for(uint32_t j=0; j<4; ++j)
-                enc_put(&codec, &table_difference, (difference>>(j*8))&0xff);
+                uint32_t bit_index = 31 - __builtin_clz(difference);
+                enc_put_bits(&codec, bit_index, 5);
+                difference &= ~(1<<bit_index);
+                bit_index = 31 - __builtin_clz(difference);
+                enc_put_bits(&codec, bit_index, 5);
+            }
+            else
+            {
+                enc_put(&codec, &block_mode, 3);
+
+                // encode the full 32 bits difference
+                for(uint32_t j=0; j<4; ++j)
+                    enc_put(&codec, &table_difference, (difference>>(j*8))&0xff);
+            }
 
             previous = current;
         }
@@ -518,11 +542,10 @@ void bc1_decrunch(const void* input, size_t length, uint32_t width, uint32_t hei
             top_table[i].key |= dec_get_bits(&codec, 8) << (j*8);
     }
 
-    range_model table_index;
+    range_model table_index, table_difference, block_mode;
     model_init(&table_index, top_table_size);
-
-    range_model table_difference;
     model_init(&table_difference, 256);
+    model_init(&block_mode, 4);
 
     bc1_block empty_block = {0};
     bc1_block* blocks = (bc1_block*)output;
@@ -552,12 +575,33 @@ void bc1_decrunch(const void* input, size_t length, uint32_t width, uint32_t hei
 
             // indices difference with top table
             uint32_t reference = dec_get(&codec, &table_index);
-            uint32_t difference=0;
-            for(uint32_t j=0; j<4; ++j)
-                difference = difference | (dec_get(&codec, &table_difference) << (j*8));
+            uint32_t mode = dec_get(&codec, &block_mode);
 
-            current->indices =  difference ^ top_table[reference].key;
+            switch(mode)
+            {
+            case 0 : current->indices = top_table[reference].key; break;
+            case 1 : 
+                {
+                    current->indices = top_table[reference].key;
+                    current->indices ^= (1 << dec_get_bits(&codec, 5));
+                    break;
+                }
+            case 2 : 
+                {
+                    current->indices = top_table[reference].key;
+                    current->indices ^= (1 << dec_get_bits(&codec, 5));
+                    current->indices ^= (1 << dec_get_bits(&codec, 5));
+                    break;
+                }
+            case 3 :
+                {
+                    uint32_t difference=0;
+                    for(uint32_t j=0; j<4; ++j)
+                        difference = difference | (dec_get(&codec, &table_difference) << (j*8));
 
+                    current->indices =  difference ^ top_table[reference].key;
+                }
+            }
             previous = current;
         }
     }
