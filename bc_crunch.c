63f40dd19e7a96e0b71c77495e302fb590cbf851
Geolm <moyaguillaume@gmail.com>
2025-11-12 12:55:28 +0100

    updated licenses

--- bc_crunch.c
+++ bc_crunch.c
@@ -1,3 +1,43 @@
+/*
+
+zlib License
+
+(C) 2025 Geolm
+
+This software is provided 'as-is', without any express or implied
+warranty.  In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+1. The origin of this software must not be misrepresented; you must not
+   claim that you wrote the original software. If you use this software
+   in a product, an acknowledgment in the product documentation would be
+   appreciated but is not required.
+2. Altered source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+3. This notice may not be removed or altered from any source distribution.
+
+
+Also as part of the code is based on FastAC, here's a copy of the license:
+
+The only purpose of this program is to demonstrate the basic principles   
+of arithmetic coding. It is provided as is, without any express or        
+implied warranty, without even the warranty of fitness for any particular 
+purpose, or that the implementations are correct.                         
+                                                                          
+Permission to copy and redistribute this code is hereby granted, provided 
+that this warning and copyright notices are not removed or altered.       
+                                                                          
+Copyright (c) 2004 by Amir Said (said@ieee.org) &                         
+                      William A. Pearlman (pearlw@ecse.rpi.edu)   
+
+*/
+
+
+
 #include <assert.h>
 #include "bc_crunch.h"
 
@@ -87,7 +127,7 @@ void model_init(range_model* model, uint32_t number_of_symbols)
 typedef struct range_codec
 {
     uint8_t *code_buffer, *ac_pointer;
-    uint32_t base, value, length;                     // arithmetic coding state
+    uint32_t base, value, length;
     uint32_t buffer_size;
 } range_codec;
 
